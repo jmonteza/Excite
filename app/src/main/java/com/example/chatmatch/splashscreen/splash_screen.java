a0725ff0bcc5969d4e0561e21c2bf19ddb56e218
Isaac samuel <osamuel@ualberta.ca>
2021-06-28 12:13:50 -0600

    (a): Splash screen added

---
+++ app/src/main/java/com/example/chatmatch/splashscreen/splash_screen.java
@@ -0,0 +1,42 @@
+package com.example.chatmatch.splashscreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.chatmatch.AuthActivity;
+import com.example.chatmatch.Matches.matches;
+import com.example.chatmatch.R;
+
+public class splash_screen extends AppCompatActivity {
+
+    
+    Thread timer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        timer = new Thread(){
+            @Override
+            public void run() {
+                try{
+                    synchronized (this){
+                        //wait for 5 seconds before proceeding
+                        wait(5000);
+                    }
+                } catch(InterruptedException e){
+                    e.printStackTrace();
+                } finally {
+                    //launch new activity / page after spash screen
+                    Intent intent = new Intent(splash_screen.this, AuthActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}
