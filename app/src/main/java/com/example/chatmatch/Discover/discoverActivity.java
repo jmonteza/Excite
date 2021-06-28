3c42f5752ea2b6be1128a5194c33879184fd3edc
Isaac samuel <osamuel@ualberta.ca>
2021-06-28 11:47:51 -0600

    (a): Added splash screen

---
+++ app/src/main/java/com/example/chatmatch/Discover/discoverActivity.java
@@ -0,0 +1,16 @@
+package com.example.chatmatch.Discover;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.chatmatch.R;
+
+public class discoverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_discover);
+    }
+}
